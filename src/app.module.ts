a71dec9f650739a0de27d0a3ab4a4f6ef6171a0a
macos <macos@MacBook-Pro-cua-macos.local>
2024-10-24 17:24:52 +0800

    config

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,14 @@ import { UsersModule } from './users/users.module';
 import { tagsModule } from './tags/tags.module';
 import { FilesModule } from './files/files.module';
 import { WallpaperModule } from './wallpaper/wallpaper.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: process.env.PATH_IMAGES,
+      serveRoot: '/uploads', // Route mà bạn muốn ánh xạ, ví dụ: /uploads/tags/Baryon Mode/...
+    }),
     ConfigModule.forRoot({
       isGlobal: true, // Để các cấu hình có sẵn trên toàn bộ ứng dụng
       load: [databaseConfig, jwtConfig],
